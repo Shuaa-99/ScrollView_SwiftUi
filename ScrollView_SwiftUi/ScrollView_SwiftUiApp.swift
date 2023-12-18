0e3fb257cd2bd8339a4b03e1b469fcfcca9d2500
Shua'a <51505005+Shuaa-99@users.noreply.github.com>
2023-12-18 16:52:07 +0300

    Initial Commit

---
+++ ScrollView_SwiftUi/ScrollView_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollView_SwiftUiApp.swift
+//  ScrollView_SwiftUi
+//
+//  Created by SHUAA on 18-12-2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrollView_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
