13624b09a785322fba8f6425c06696def04971fa
Shua'a <51505005+Shuaa-99@users.noreply.github.com>
2023-12-18 18:45:18 +0300

    .

--- ScrollView_SwiftUi/ContentView.swift
+++ ScrollView_SwiftUi/ContentView.swift
@@ -6,18 +6,72 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    let images = ["circle.fill","square.fill","rectangle.fill","triangle.fill","diamond.fill"]
+    let shapeName = ["circle","square","rectangle","triangle","diamond"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            //horizontal
+            ScrollView(.horizontal,showsIndicators: false){
+                HStack(spacing: 10){
+                    ForEach(0..<images.count,id: \.self){ number in
+                        VStack(alignment: .leading,spacing: 16.0){
+                            Image(systemName: "\(images[number])")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 200,height: 200)
+                            VStack{
+                                
+                                Text("\(shapeName[number])")
+                                    .font(.headline)
+    
+                            }.padding(.bottom)
+                            .padding(.horizontal)
+                            
+                        }
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 24.0))
+                        .shadow(radius: 8)
+                        .frame(width: 212,height: 350)
+                        
+                    }
+                }
+                .padding(.horizontal,20)
+            }
+            
+            //vertical
+                ScrollView(.vertical,showsIndicators: false){
+                    VStack(spacing: 10){
+                        ForEach(0..<images.count,id: \.self){ number in
+                            VStack(alignment: .leading,spacing: 16.0){
+                                Image(systemName: "\(images[number])")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 200,height: 200)
+                                VStack{
+                                    
+                                    Text("\(shapeName[number])")
+                                        .font(.headline)
+                                    
+                                }.padding(.bottom)
+                                    .padding(.horizontal)
+                                
+                            }
+                            .background(Color.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 24.0))
+                            .shadow(radius: 8)
+                            .frame(width: 212,height: 350)
+                            
+                        }
+                    .padding(.horizontal,20)
+                }
+            }
+
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
